6e08e00d6664e10b0c57040713e5c2793fbd942f
italomaia03 <italo.fernandes.om@gmail.com>
2023-07-26 16:44:38 -0300

    feat: implement validation schema for tutor creation

--- src/models/validators/tutor/TutorValidator.create.ts
+++ src/models/validators/tutor/TutorValidator.create.ts
@@ -1,9 +1,11 @@
 import Joi from "joi";
+import { ITutor } from "../../interfaces/ITutor";
 
-export const tutorCreateValidationSchema = Joi.object({
+export const tutorCreateValidationSchema = Joi.object<ITutor>({
     name: Joi.string().required(),
     password: Joi.string().required(),
-    phone: Joi.string().pattern(/[0-9](11)/),
+    phone: Joi.string().pattern(/^\d{11}$/),
     email: Joi.string().email({ minDomainSegments: 2 }).required(),
     date_of_birth: Joi.date().required(),
+    zip_code: Joi.string().pattern(/^\d{8}/),
 });
