415c8bb262167e65b2ac0440e043e92657c161c8
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2023-07-26 10:24:55 -0400

    feat: port configuration

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+export{app}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
+import { app } from "./app";
 
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        app.listen(port, () => console.log(`Server is listening port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
