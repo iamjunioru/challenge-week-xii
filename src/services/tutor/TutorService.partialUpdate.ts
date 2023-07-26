3700431a5fa44611b2fb2271559fba17fe89955a
Junior <iamjunioru@gmail.com>
2023-07-26 20:20:39 -0300

    feat: implement service to PATCH tutor

---
+++ src/services/tutor/TutorService.partialUpdate.ts
@@ -0,0 +1,17 @@
+import { Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { ITutor } from "../../models/interfaces/ITutor";
+import { partialUpdateTutor } from "../../repository/tutor/TutorRepository.partialUpdate";
+
+export async function partialUpdateTutorService(
+  tutorId: string,
+  updates: ITutor,
+  res: Response
+) {
+  try {
+    const updatedTutor = await partialUpdateTutor(tutorId, updates);
+    res.status(StatusCodes.OK).json(updatedTutor);
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Failed to update tutor" });
+  }
+}
