289dd394d385e546dffa808c8dfaad16a73c05d9
Junior <iamjunioru@gmail.com>
2023-07-26 14:37:29 -0300

    feat(tutorRoute): add tutor route base

---
+++ src/routes/tutorRoute.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const route = express.Router();
+
+route.get("/tutors", );
+route.post("/auth", );
+route.post("/tutor", );
+route.put("/tutor/:id", );
+route.patch("/tutor/:id", /* PATCH to update tutor specific information */);
+route.delete("/tutor/:id", );
+
+export { route };
