d5d57da328ba876a193c6d0b6a619dae70af6689
Junior <iamjunioru@gmail.com>
2023-07-26 20:26:33 -0300

    feat: PATCH route creation

--- src/routes/tutorRoute.ts
+++ src/routes/tutorRoute.ts
@@ -2,18 +2,21 @@ import express from "express";
 import { tutorCreateValidator } from "../middlewares/validators/tutorCreateValidator";
 import { createTutorController } from "../controllers/tutor/TutorController.create";
 import { hashTutorPassword } from "../middlewares/hashTutorPassword";
+import { partialUpdateTutorController } from "../controllers/tutor/TutorController.partialUpdate";
 
 const route = express.Router();
 
 route.get("/tutors");
 route.post(
-    "/tutor",
-    tutorCreateValidator,
-    hashTutorPassword,
-    createTutorController
+  "/tutor",
+  tutorCreateValidator,
+  hashTutorPassword,
+  createTutorController
 );
 route.put("/tutor/:id");
-route.patch("/tutor/:id" /* PATCH to update tutor specific information */);
-route.delete("/tutor/:id");
+route.patch(
+    "/tutor/:id",
+     partialUpdateTutorController);
+route.delete("/tutor/:id", );
 
 export { route };
