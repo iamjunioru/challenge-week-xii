05ec39a1b463b49d17db0886c3a46ce6c8c7ac0e
italomaia03 <italo.fernandes.om@gmail.com>
2023-07-26 17:25:41 -0300

    feat: add password hashing middleware to post route

--- src/routes/tutorRoute.ts
+++ src/routes/tutorRoute.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { tutorCreateValidator } from "../middlewares/validators/tutorCreateValidator";
 import { createTutorController } from "../controllers/tutor/TutorController.create";
+import { hashTutorPassword } from "../middlewares/hashTutorPassword";
 
 const route = express.Router();
 
 route.get("/tutors");
-route.post("/tutor", tutorCreateValidator, createTutorController);
+route.post(
+    "/tutor",
+    tutorCreateValidator,
+    hashTutorPassword,
+    createTutorController
+);
 route.put("/tutor/:id");
 route.patch("/tutor/:id" /* PATCH to update tutor specific information */);
 route.delete("/tutor/:id");
