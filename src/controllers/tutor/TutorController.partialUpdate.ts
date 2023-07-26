ed37f6350433810a3e7d3022389e581e45cd2c40
Junior <iamjunioru@gmail.com>
2023-07-26 20:13:53 -0300

    fix: import fix

--- src/controllers/tutor/TutorController.partialUpdate.ts
+++ src/controllers/tutor/TutorController.partialUpdate.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from "express";
-import { tutorPartialUpdateValidator } from "../../middlewares/validators/tutorPartialUpdateValidator";
-import { partialUpdateTutorService } from "../../services/tutor/TutorService.partialUpdate";
+    import { Request, Response } from "express";
+    import { tutorPatchValidator } from "../../middlewares/validators/tutorPartialUpdateValidator";
+    import { partialUpdateTutorService } from "../../services/tutor/TutorService.partialUpdate";
 
-export async function partialUpdateTutorController(req: Request, res: Response) {
-  const tutorId = req.params.id;
-  const updates = req.body;
+    export async function partialUpdateTutorController(req: Request, res: Response) {
+    const tutorId = req.params.id;
+    const updates = req.body;
 
-  await tutorPartialUpdateValidator(req, res, async () => {
-    await partialUpdateTutorService(tutorId, updates, res);
-  });
-}
+    await tutorPatchValidator(req, res, async () => {
+        await partialUpdateTutorService(tutorId, updates, res);
+    });
+    }
