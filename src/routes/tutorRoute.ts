ba739f10e060129cb3867b140ed694ceaf1dc697
italomaia03 <italo.fernandes.om@gmail.com>
2023-07-26 16:46:19 -0300

    feat: add middlewares to tutor post route

--- src/routes/tutorRoute.ts
+++ src/routes/tutorRoute.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import express from "express";
+import { tutorCreateValidator } from "../middlewares/validators/tutorCreateValidator";
+import { createTutorController } from "../controllers/tutor/TutorController.create";
 
 const route = express.Router();
 
-route.get("/tutors", );
-route.post("/tutor", );
-route.put("/tutor/:id", );
-route.patch("/tutor/:id", /* PATCH to update tutor specific information */);
-route.delete("/tutor/:id", );
+route.get("/tutors");
+route.post("/tutor", tutorCreateValidator, createTutorController);
+route.put("/tutor/:id");
+route.patch("/tutor/:id" /* PATCH to update tutor specific information */);
+route.delete("/tutor/:id");
 
 export { route };
