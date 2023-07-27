507e943efa49d6ce1d7bf87bb3fd6c4e4955005a
italomaia03 <italo.fernandes.om@gmail.com>
2023-07-27 17:58:03 -0300

    feat: link new pet to desired tutor

---
+++ src/services/pet/PetService.create.ts
@@ -0,0 +1,27 @@
+import { IPet } from "../../models/interfaces/IPet";
+import { createPet } from "../../repository/pet/PetRepository.create";
+import { Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { getTutor } from "../tutor/TutorService.getOne";
+import { partialUpdateTutor } from "../../repository/tutor/TutorRepository.partialUpdate";
+import { ITutor } from "../../models/interfaces/ITutor";
+
+export async function createPetService(
+    tutorId: string,
+    pet: IPet,
+    res: Response
+) {
+    const desiredTutor = await getTutor(tutorId);
+
+    if (!desiredTutor) {
+        res.status(StatusCodes.NOT_FOUND).json({ msg: "Tutor not found" });
+    }
+
+    const createdPet = await createPet(pet);
+
+    desiredTutor!.pets.push(createdPet);
+
+    await partialUpdateTutor(tutorId, desiredTutor as ITutor);
+
+    res.status(StatusCodes.CREATED).json(createdPet);
+}
