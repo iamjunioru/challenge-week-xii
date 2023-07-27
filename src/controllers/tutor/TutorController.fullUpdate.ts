3415cf5b2cbf8e7eb2d39c10a9746b34fa84abab
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2023-07-27 10:08:36 -0400

    fix: tutor fullUpdateController

--- src/controllers/tutor/TutorController.fullUpdate.ts
+++ src/controllers/tutor/TutorController.fullUpdate.ts
@@ -5,24 +5,22 @@ import { FullUpdateTutor } from "../../services/tutor/TutorService.fullUpdate";
 const fullUpdateService = new FullUpdateTutor();
 
 async function putTutorsController (req: Request, res: Response) {
-    try {
-        const idTutor = req.params.id;
-        const tutorData = req.body;
 
-        const tutor = await fullUpdateService.putTutors(idTutor, tutorData);
+    const idTutor = req.params.id;
+    const tutorData = req.body;
 
-        if(!tutor){
-            res.status(StatusCodes.BAD_REQUEST).json({msg: `No tutor with id ${idTutor}`});
-        }
-        res.status(StatusCodes.OK).json({name: tutor!.name, 
-            phone: tutor!.phone, 
-            email: tutor!.email, 
-            date_of_birth: tutor!.date_of_birth, 
-            zip_code: tutor!.zip_code
-        });
-    } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg: error});
+    const tutor = await fullUpdateService.putTutors(idTutor, tutorData);
+
+    if(!tutor){
+        res.status(StatusCodes.BAD_REQUEST).json({msg: `No tutor with id ${idTutor}`});
     }
+    res.status(StatusCodes.OK).json({name: tutor!.name, 
+        phone: tutor!.phone, 
+        email: tutor!.email, 
+        date_of_birth: tutor!.date_of_birth, 
+        zip_code: tutor!.zip_code
+    });
+
 };
 
 export {putTutorsController}
