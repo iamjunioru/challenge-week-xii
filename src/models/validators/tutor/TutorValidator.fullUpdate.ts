f61b68fc5a7d914a353f08f621768510fb55fb1d
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2023-07-27 09:04:47 -0400

    feat: creating PUT route validation

---
+++ src/models/validators/tutor/TutorValidator.fullUpdate.ts
@@ -0,0 +1,10 @@
+import Joi from "joi";
+import { ITutor } from "../../interfaces/ITutor";
+
+export const tutorFullUpdateValidationSchema = Joi.object<ITutor>({
+    name: Joi.string().required(),
+    phone: Joi.string().pattern(/^\d{11}$/),
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    date_of_birth: Joi.date().required(),
+    zip_code: Joi.string().pattern(/^\d{8}/),
+});
