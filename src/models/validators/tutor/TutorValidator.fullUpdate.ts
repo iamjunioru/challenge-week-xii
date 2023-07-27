c4634798dc66a0297f64a3f391a5fd65c6d2f5a9
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2023-07-27 09:25:29 -0400

    fix: tutorFullUpdateValidation

--- src/models/validators/tutor/TutorValidator.fullUpdate.ts
+++ src/models/validators/tutor/TutorValidator.fullUpdate.ts
@@ -3,8 +3,8 @@ import { ITutor } from "../../interfaces/ITutor";
 
 export const tutorFullUpdateValidationSchema = Joi.object<ITutor>({
     name: Joi.string().required(),
-    phone: Joi.string().pattern(/^\d{11}$/),
+    phone: Joi.string().pattern(/^\d{11}$/).required(),
     email: Joi.string().email({ minDomainSegments: 2 }).required(),
     date_of_birth: Joi.date().required(),
-    zip_code: Joi.string().pattern(/^\d{8}/),
+    zip_code: Joi.string().pattern(/^\d{8}/).required(),
 });
