cf343b9b19e0e3076a6a2a602b436ef83c4a3965
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2023-07-28 13:47:24 -0400

    refactor: "putFullPetRepository"

--- src/repository/pet/PetRepository.fullUpdate.ts
+++ src/repository/pet/PetRepository.fullUpdate.ts
@@ -1,33 +1,8 @@
-import { Pet } from "../../models/Pet"
 import { Tutor } from "../../models/Tutor";
 import { IPet } from "../../models/interfaces/IPet";
-import { ITutor } from "../../models/interfaces/ITutor";
-
-
 
 export class PetRepository{
-    async updatePet(idP: string, idT: string, petData: IPet) {
-        const tutor = await Tutor.findById(idT);
-
-        if (!tutor) {
-            return false
-        } 
-
-        const pet = await tutor.pets.id(idP);
-
-        if (!pet) {
-            return false
-        }
-
-        pet.name = petData.name;
-        pet.species = petData.species;
-        pet.carry = petData.carry;
-        pet.weight = petData.weight;
-        pet.date_of_birth = petData.date_of_birth;
-
-        pet.set(petData);
-        await tutor.save();
-
-        return pet
+    async updatePet(idPet: string, idTutor: string, petData: IPet) {
+        return await Tutor.findById(idPet, idTutor, petData);
     }
 }
