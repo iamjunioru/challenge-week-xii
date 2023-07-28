20b9aa3d67ad8b2f984829637a3e78e33d0a00e2
italomaia03 <italo.fernandes.om@gmail.com>
2023-07-28 16:20:48 -0300

    feat: implement new logic to show desired response

--- src/repository/tutor/TutorRepository.listAll.ts
+++ src/repository/tutor/TutorRepository.listAll.ts
@@ -1,9 +1,44 @@
-import { Tutor } from "../../models/Tutor"
+import { Tutor } from "../../models/Tutor";
 
-
-export class TutorRepository{
-
-    async getAll(){
-        return Tutor.find({}, '-password')
+export class TutorRepository {
+    async getAll() {
+        const pipeline = [
+            {
+                $set: {
+                    date_of_birth: {
+                        $dateToString: {
+                            format: "%Y-%m-%d %H:%M",
+                            date: "$date_of_birth",
+                        },
+                    },
+                },
+            },
+            {
+                $project: { password: false },
+            },
+        ];
+        return await Tutor.aggregate(pipeline)
+            .lookup({
+                from: "pets",
+                localField: "_id",
+                foreignField: "tutorId",
+                as: "pets",
+                pipeline: [
+                    {
+                        $set: {
+                            date_of_birth: {
+                                $dateToString: {
+                                    format: "%Y-%m-%d %H:%M",
+                                    date: "$date_of_birth",
+                                },
+                            },
+                        },
+                    },
+                ],
+            })
+            .project({
+                "pets.tutorId": false,
+                "tutors.password": false,
+            });
     }
 }
