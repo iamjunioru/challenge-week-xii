3a5ac7e8f01e50a2cca959a2a4104adbae598b16
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2023-07-28 13:48:06 -0400

    refactor: "putFullPetService"

--- src/services/pet/PetService.fullUpdate.ts
+++ src/services/pet/PetService.fullUpdate.ts
@@ -2,6 +2,7 @@ import { PetRepository } from "../../repository/pet/PetRepository.fullUpdate";
 import { IPet } from "../../models/interfaces/IPet";
 import { Response } from "express";
 import {pick} from "lodash";
+import { Tutor } from "../../models/Tutor";
 
 
 
@@ -12,13 +13,35 @@ export class PetFullUpdateService{
         this.petRepository = new PetRepository();
     }
 
-    async putPets(idP: string, idT: string, petData: IPet, res: Response){ 
-        
-        const updatedPet = await this.petRepository.updatePet(idP, idT, petData);
+    async updateFullPet(idPet: string, idTutor: string, petData: IPet){
+        const tutor = await Tutor.findById(idTutor);
 
-        if(!updatedPet){
-            throw new Error(`No pet with id ${idP} or tutor with id ${idT}`);
+        if (!tutor) {
+            throw new Error(` No tutor with id ${idTutor}`)
         }
+
+        const pet = tutor.pets.id(idPet);
+
+        if (!pet) {
+            throw new Error(`No pet with id ${idPet}`);
+        } 
+
+        pet.name = petData.name;
+        pet.species = petData.species;
+        pet.carry = petData.carry;
+        pet.weight = petData.weight;
+        pet.date_of_birth = petData.date_of_birth;
+
+        pet.set(petData);
+        await tutor.save();
+
+        return pet
+    }
+
+    async putPets(idPet: string, idTutor: string, petData: IPet, res: Response){
+        await this.updateFullPet(idPet, idTutor, petData);
+        
+        const updatedPet = await this.petRepository.updatePet(idPet, idTutor, petData);
 
         const petWithoutId = pick(updatedPet, ['name', 'species', 'carry', 'weight', 'date_of_birth']);
 
