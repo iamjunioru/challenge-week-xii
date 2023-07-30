111dface16c4ed23d55438a021fadd19efa90b37
Anderson Menezes <Andersonfielads2@gmail.com>
2023-07-29 22:43:12 -0300

    style: deleteTutor controller

--- src/controllers/tutor/TutorController.delete.ts
+++ src/controllers/tutor/TutorController.delete.ts
@@ -1,27 +1,15 @@
-import { StatusCodes } from "http-status-codes";
 import  {Request,Response} from "express";
-import {Tutor} from "../../models/Tutor";
+import { deleteTutorService } from "../../services/tutor/TutorService.delete";
 
-export async function deleteTutor(req:Request,res:Response) {
-
-    try {
-
-        const id = req.params.id;
-        const tutor = await Tutor.findById(id);
-
-        if(!tutor) {
-            res.status(StatusCodes.BAD_REQUEST).json({msg: `No tutor with id ${id}`});
-        }
-        if (tutor.pets.length > 0) {
-            res.status(StatusCodes.BAD_REQUEST).json({msg:`Tutor have an pet delete this first`})
-        }
-
-        await tutor.deleteOne()
-        res.status(StatusCodes.OK).json({msg:`Tutor has been deleted`});
-        
-    } catch (e:any) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:`${e}`});
-        
-    }
-    
+async function deleteTutorController(req:Request,res:Response) {
+try {
+    const id = req.params.id;
+    const result =await deleteTutorService(id)
+    if(result.sucess) return res.status(202).json({messege:result.msg})
+    return res.status(500).json({messege:result.msg})
+} catch (e) {
+    return res.status(500).json(`Internal error`)
+}
 };
+
+export {deleteTutorController}
