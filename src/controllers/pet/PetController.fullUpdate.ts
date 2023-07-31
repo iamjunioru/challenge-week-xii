315a35bf95e21764b0abbf4124bb41c0df0c75e1
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2023-07-31 16:26:20 -0400

    refactor: "changing quotes"

--- src/controllers/pet/PetController.fullUpdate.ts
+++ src/controllers/pet/PetController.fullUpdate.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from 'express';
-import { PetFullUpdateService } from '../../services/pet/PetService.fullUpdate';
-import { IPet } from '../../models/interfaces/IPet';
+import { Request, Response } from "express";
+import { PetFullUpdateService } from "../../services/pet/PetService.fullUpdate";
+import { IPet } from "../../models/interfaces/IPet";
 
 const petFullUpdateService = new PetFullUpdateService();
 
--- src/repository/tutor/TutorRepository.fullUpdate.ts
+++ src/repository/tutor/TutorRepository.fullUpdate.ts
@@ -1,11 +1,5 @@
-import { Tutor } from '../../models/Tutor';
-
+import { Tutor } from "../../models/Tutor";
 export class TutorRepositoryFullUpdate{
-    static mockReturnValue(fullUpdateService: jest.Mock<any, any, any>) {
-        throw new Error("Method not implemented.");
-        return false;
-    }
-
     async updateTutor(id: string, tutorData: any) {
         return Tutor.findByIdAndUpdate({_id: id}, tutorData, {
             new: true,
